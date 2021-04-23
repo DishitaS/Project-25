0dd86587acbf9df1888a570a888cb29a6646eba0
DishitaS <76867580+DishitaS@users.noreply.github.com>
2021-04-23 17:06:27 +0530

    Add files via upload

---
+++ Project 25/paper.js
@@ -0,0 +1,12 @@
+class Paper{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            density:1.2,
+            friction:0,
+        }
+
+        this.image=loadImage("paper.png")
+    }
+}
